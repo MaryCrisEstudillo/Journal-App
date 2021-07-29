ebc0a5c27ed5141ca561ea319eee58a2bce0acdd
MaryCrisEstudillo <estudillomarycris@gmail.com>
2021-07-29 16:49:16 +0800

    Add create and show in controller w/ methods

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -3,4 +3,14 @@ class CategoryController < ApplicationController
     def new
         @category = Category.new
     end
+    
+    def create
+        @category = Category.create(category_params)
+        redirect_to @category
+    end
+
+private
+    def category_params
+        params.require(:category).permit(:title, :body)
+    end
 end

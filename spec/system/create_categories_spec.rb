58f970dfebadf6d88a1cdf066f633e69c8933208
MaryCrisEstudillo <estudillomarycris@gmail.com>
2021-07-29 15:45:34 +0800

    Added resource

---
+++ spec/system/create_categories_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "CreateCategories", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  it 'saves and displays the resulting Categories' do
+      visit '/category/new'
+  
+      fill_in 'Title', with: 'Hello, World!'
+      fill_in 'Body', with: 'Hello, I say!'
+    
+      click_on 'ADD'
+    
+      expect(page).to have_content('Hello, World!')
+      expect(page).to have_content('Hello, I say!')
+    
+      category = Category.order("id").last
+      expect(category.title).to eq('Hello, World!')
+      expect(category.body).to eq('Hello, I say!')
+  end
+end
